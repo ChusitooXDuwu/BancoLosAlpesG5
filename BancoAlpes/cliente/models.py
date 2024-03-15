3b045013572d25db1ac60a8034363aa26a58fd38
Wyo Hann Chu <w.chu@uniandes.edu.co>
2024-03-14 19:48:10 -0500

    Modelos updated, admin updated, realtion added

--- BancoAlpes/cliente/models.py
+++ BancoAlpes/cliente/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Cliente(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    cedula = models.CharField(max_length=10)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=10)
+    direccion = models.CharField(max_length=100)
+    fecha_nacimiento = models.DateField()
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    estado_docuemntos = models.CharField(max_length=50)
+    
+
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'
+
--- BancoAlpes/documents/models.py
+++ BancoAlpes/documents/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from cliente.models import Cliente
 
+class Documento(models.Model):
+    id = models.AutoField(primary_key=True)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, default=None)
+    tipo = models.CharField(max_length=50)
+    fecha_subida = models.DateTimeField(auto_now_add=True)
+    estado = models.CharField(max_length=50)   #si ya fue confirmado o no
+    archivo = models.FileField(upload_to='documentos/')
+    score_confiabilidad = models.FloatField(default=0.0)  #score ese importante para analisis debe ser x un api hay que mockearlo
+    
+
+    def __str__(self):
+        return f'{self.cliente} - {self.tipo} - {self.estado}'
 # Create your models here.
