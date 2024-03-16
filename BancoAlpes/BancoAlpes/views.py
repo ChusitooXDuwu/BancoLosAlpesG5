856d3f4989c8aab4abaa558ad1c4afdd932aa9aa
Jeronimo <j.vargasr2@uniandes.edu.co>
2024-03-15 23:06:35 -0500

    HC

--- BancoAlpes/BancoAlpes/views.py
+++ BancoAlpes/BancoAlpes/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
     return HttpResponse("Hello world! Django views")
+
+def healthCheck(request):
+    return HttpResponse("ok")
 
 #def fucntion that callls  pdf from folder docClientes
