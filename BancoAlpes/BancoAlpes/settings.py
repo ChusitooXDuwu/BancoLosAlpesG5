e0cc513b76d7c8020c7faf8d7e2b61c93a062a2e
Wyo Hann Chu <w.chu@uniandes.edu.co>
2024-03-16 00:39:16 -0500

    added get all documents

--- BancoAlpes/BancoAlpes/settings.py
+++ BancoAlpes/BancoAlpes/settings.py
@@ -82,10 +82,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'banco_db',
-        'USER': 'chu',
+        'USER': 'postgres',
         'PASSWORD': 'chu',
-        'HOST': '10.128.0.3',
-        'PORT': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- BancoAlpes/documents/logic/logic_documents.py
+++ BancoAlpes/documents/logic/logic_documents.py
@@ -3,3 +3,7 @@ from ..models import Documento
 def get_document(document_pk):
     document = Documento.objects.get(pk=document_pk)
     return document
+
+def get_documents():
+    documents = Documento.objects.all()
+    return documents
