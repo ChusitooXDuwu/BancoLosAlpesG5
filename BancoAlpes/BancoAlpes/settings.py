a518f19e5354648473b74caf398d69429e223f09
Jeronimo <j.vargasr2@uniandes.edu.co>
2024-03-15 23:37:48 -0500

    File

--- BancoAlpes/BancoAlpes/settings.py
+++ BancoAlpes/BancoAlpes/settings.py
@@ -12,11 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DOC_ROOT = os.path.join(BASE_DIR, 'docsClientes')
-
 DOC_URL = '/docsClientes/'
 
 # Quick-start development settings - unsuitable for production
--- BancoAlpes/BancoAlpes/views.py
+++ BancoAlpes/BancoAlpes/views.py
@@ -7,4 +7,17 @@ def home(request):
 def healthCheck(request):
     return HttpResponse("ok")
 
+# Render all kind of files
+def pdf_view(request, *args, **kwargs):
+    try:
+        path = kwargs.get('path', '')
+        with open(f'docClientes/{path}', 'rb') as pdf:
+            response = HttpResponse(pdf.read(), content_type='application/pdf')
+            response['Content-Disposition'] = 'inline;filename=some_file.pdf'
+            return response
+    except FileNotFoundError:
+        return HttpResponse("The file was not found")
+
+
+
 #def fucntion that callls  pdf from folder docClientes
