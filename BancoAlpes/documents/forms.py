b03b1d9684547f27122e13886470617bff4d09e5
Wyo Hann Chu <w.chu@uniandes.edu.co>
2024-04-11 18:37:06 -0500

    bu

--- BancoAlpes/documents/forms.py
+++ BancoAlpes/documents/forms.py
@@ -6,17 +6,17 @@ class DocumentForm(forms.ModelForm):
     class Meta:
         model = Documento
         fields = [
-            'Cliente',
-            'Tipo',
-            'Estado',
-            'Archivo',
-            'Score confiabilidad',
+            'cliente',
+            'tipo',
+            'estado',
+            'archivo',
+            'score confiabilidad',
         ]
 
         labels = {
-            'Cliente' : 'Cliente',
-            'Tipo' : 'Tipo',
-            'Estado' : 'Estado',
-            'Archivo' : 'Archivo',
-            'Score confiabilidad' : 'Score confiabilidad',
+            'cliente' : 'Cliente',
+            'tipo' : 'Tipo',
+            'estado' : 'Estado',
+            'archivo' : 'Archivo',
+            'score confiabilidad' : 'Score Confiabilidad',
         }
